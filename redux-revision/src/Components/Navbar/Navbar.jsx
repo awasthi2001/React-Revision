3086b071f29f83cc22e8fdeefc0adb6ac22cc2aa
awasthi2001 <akshanshawasthi013@gmail.com>
2023-01-26 06:58:08 +0530

    added logout functionality

--- redux-revision/src/Components/Navbar/Navbar.jsx
+++ redux-revision/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 import styles from './navbar.module.css'
-import {useDispatch} from 'react-redux'
+import {useDispatch, useSelector} from 'react-redux'
 import {set_login} from '../../Redux/Authentication/action.js'
 const Navbar = () => {
+  let {isAuth} = useSelector(state=>state.Auth)
   const dispatch = useDispatch()
   console.log(dispatch)
   function handleLogout(){
@@ -14,10 +15,13 @@ const Navbar = () => {
         <div className={styles.Nav_Container} >
            <NavLink className={(Active)=>{
            return Active.isActive?styles.Active:styles.isNotActive}} to='/' >HOME</NavLink>
-           <NavLink className={(Active)=>{
-           return Active.isActive?styles.Active:styles.isNotActive}} to='/login'>LOGIN</NavLink>
-           <NavLink className={(Active)=>{
-           return Active.isActive?styles.Active:styles.isNotActive}}  to='/SignUp' >SIGNUP</NavLink>
+           {
+            isAuth ? <button onClick={handleLogout} >LOGOUT</button> :       <><NavLink className={(Active)=>{
+              return Active.isActive?styles.Active:styles.isNotActive}} to='/login'>LOGIN</NavLink>
+              <NavLink className={(Active)=>{
+              return Active.isActive?styles.Active:styles.isNotActive}}  to='/SignUp' >SIGNUP</NavLink></>    
+           }
+ 
         </div>
     </div>
   )
